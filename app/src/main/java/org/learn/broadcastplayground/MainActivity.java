0b7d33f9bedd7258ada5f92d125eb35fae3562ef
prashmittanay <prashmittanay@gmail.com>
2020-10-02 10:23:59 +0530

    Dynamic Broadcast Receiver intercepting connectivity changes implemented

--- app/src/main/java/org/learn/broadcastplayground/MainActivity.java
+++ app/src/main/java/org/learn/broadcastplayground/MainActivity.java
@@ -2,13 +2,39 @@ package org.learn.broadcastplayground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private  DynamicReceiverOne mDynamicReceiverOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDynamicReceiverOne = new DynamicReceiverOne();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        bindBroadcastReceivers();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(mDynamicReceiverOne);
+    }
+
+    private void bindBroadcastReceivers() {
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(mDynamicReceiverOne, intentFilter);
     }
 }
